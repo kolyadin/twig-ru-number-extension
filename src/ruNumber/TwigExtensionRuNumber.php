c5129b006d7d86b60a24ee57590ae9217c9b1ad9
Aleksey Kolyadin <donflash@gmail.com>
2014-01-20 16:31:41 +0400

    Исправлен вывод с "0"

--- src/ruNumber/TwigExtensionRuNumber.php
+++ src/ruNumber/TwigExtensionRuNumber.php
@@ -20,6 +20,8 @@ class TwigExtensionRuNumber extends \Twig_Extension{
 
 		$pluralNum = ($n%10==1 && $n%100!=11 ? 0 : $n%10>=2 && $n%10<=4 && ($n%100<10 || $n%100>=20) ? 1 : 2);
 
+		if ($n == 1) $pluralNum = 0;
+
 		return sprintf('%d %s', $n, func_get_args()[1][$pluralNum]);
 	}
 
