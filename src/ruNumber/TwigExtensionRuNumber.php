83ce198a93313d7ea6e8d131000dd63e523dbd7a
Aleksey Kolyadin <donflash@gmail.com>
2014-02-14 19:31:22 +0400

    Добавлена поддержка пользовательского вывода данных

--- src/ruNumber/TwigExtensionRuNumber.php
+++ src/ruNumber/TwigExtensionRuNumber.php
@@ -15,15 +15,25 @@ class TwigExtensionRuNumber extends \Twig_Extension{
 
 	}
 
+//	kid.commentsCount|ruNumber('Получилось %d %s',['комментарий','комментария','комментариев'])
 	public function ruNumber(){
-		$n = func_get_args()[0];
+		$args = func_get_args();
+
+		$n = $args[0];
 
 		$pluralNum = ($n%10==1 && $n%100!=11 ? 0 : $n%10>=2 && $n%10<=4 && ($n%100<10 || $n%100>=20) ? 1 : 2);
 
 		if ($n == 1) $pluralNum = 0;
 
+		if (is_string($args[1])){
 
-		return sprintf('%d %s', $n, func_get_args()[1][$pluralNum]);
+			return strtr($args[1],[
+				'%d' => $n,
+				'%s' => $args[2][$pluralNum]
+			]);
+
+		}elseif (is_array($args[1])){
+			return sprintf('%d %s', $n, $args[1][$pluralNum]);
+		}
 	}
-
 }
